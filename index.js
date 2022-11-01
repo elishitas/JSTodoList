c7366d279689d7a6cecd3ef6e019a557a7518b9a
Elishitas <arayamariaelisa@gmail.com>
2022-10-31 23:04:10 -0300

    html

---
+++ index.js
@@ -0,0 +1,12 @@
+document.addEventListener('DOMContentLoaded', function(){
+    const title = document.getElementById('title');
+    const description = document.getElementById('description');
+    const btn = document.getElementById('add');
+
+    btn.onclick = function() {
+        console.log('title', title.value)
+        console.log('description', description.value)
+    }
+})
+
+
